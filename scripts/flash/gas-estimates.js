802d304703d17438686ff3372a6c9bb4c32d3c76
Tom Haile <bradford.haile@gmail.com>
2018-03-28 19:51:07 -0500

    activate all estimate methods

--- scripts/flash/gas-estimates.js
+++ scripts/flash/gas-estimates.js
@@ -9,6 +9,57 @@ var getRepTokens = require("./get-rep-tokens");
 var cannedMarketsData = require("../dp/data/canned-markets");
 var immutableDelete = require("immutable-delete");
 var displayTime = require("./display-time");
+var getPayoutNumerators = require("./get-payout-numerators");
+
+function getMarket(augur, universe, marketType, callback) {
+  augur.markets.getMarkets({ universe: universe}, function (err, marketIds) {
+    if (err) return callback(err);
+    augur.markets.getMarketsInfo({ marketIds: marketIds }, function (err, marketInfos) {
+      if (err) return callback(err);
+      callback(null, marketInfos.find(function (marketInfo) { return marketInfo.marketType === marketType; }));
+    });
+  });
+}
+
+function estimateFinalizeMarket(augur, universe, auth, callback) {
+  getMarket(augur, universe, "binary", function (err, market) {
+    augur.api.Market.finalize({ tx: { to: market.id, estimateGas: true, gas: augur.constants.DEFAULT_MAX_GAS },
+      onSent: function () { },
+      onSuccess: function (result) {
+        console.log(chalk.red("Finalize Market"));
+        console.log(chalk.green.dim("gas Result"), chalk.green(JSON.stringify(result)));
+        callback(null, result);
+      },
+      onFailed: function (err) {
+        console.log(chalk.red("Finalize Market"));
+        console.log(chalk.red.dim("Failed:"), chalk.red(JSON.stringify(err)));
+        callback(err);
+      },
+    });
+  });
+}
+function estimateInitialReport(augur, universe, auth, timestamp, callback) {
+  getMarket(augur, universe, "binary", function (err, market) {
+    var payoutNumerators = getPayoutNumerators(market, 1, false);
+    augur.api.Market.doInitialReport({
+      meta: auth,
+      tx: { to: market.id, estimateGas: true, gas: augur.constants.DEFAULT_MAX_GAS },
+      _payoutNumerators: payoutNumerators,
+      _invalid: false,
+      onSent: function () { },
+      onSuccess: function (result) {
+        console.log(chalk.red("Initial Report"));
+        console.log(chalk.green.dim("gas Result"), chalk.green(JSON.stringify(result)));
+        callback(null, result);
+      },
+      onFailed: function (err) {
+        console.log(chalk.red("Initial Report"));
+        console.log(chalk.red.dim("Failed:"), chalk.red(JSON.stringify(err)));
+        callback(err);
+      },
+    });
+  });
+}
 
 function estimateCreateBinaryMarket(augur, universe, auth, timestamp, callback) {
   var amount = 10000;
@@ -30,20 +81,20 @@ function estimateCreateBinaryMarket(augur, universe, auth, timestamp, callback) 
       _feePerEthInWei: "0x123456",
       _denominationToken: augur.contracts.addresses[augur.rpc.getNetworkID()].Cash,
       _designatedReporterAddress: auth.address,
-      tx: {estimateGas: true},
-      onSent: function (res) {
-        console.log(chalk.green.dim("createMarket gas estimate sent:"), chalk.green(res.hash));
-      },
-      onSuccess: function (res) {
-        console.log(chalk.green.dim("gas result"), chalk.green(JSON.stringify(res)));
-        callback(res);
+      tx: {estimateGas: true, call: true, send: false, gas: augur.constants.DEFAULT_MAX_GAS },
+      onSent: function () { },
+      onSuccess: function (result) {
+        console.log(chalk.red("Create Binary Market"));
+        console.log(chalk.green.dim("gas result"), chalk.green(JSON.stringify(result)));
+        callback(result);
       },
       onFailed: function (err) {
+        console.log(chalk.red("Create Binary Market"));
         console.log(chalk.red(JSON.stringify(err)));
         callback(err);
       },
     });
-    console.log(JSON.stringify(createMarketParams));
+    // console.log(JSON.stringify(createMarketParams));
     createMarketOfType(createMarketParams);
   });
 }
@@ -56,6 +107,18 @@ function gasEstimatesInternal(augur, universe, auth, callback) {
     displayTime("Current Time:", timestamp);
     if (err) return callback(err);
     async.parallel({
+      finalizeMarket: function (next) {
+        estimateFinalizeMarket(augur, universe, auth, function (err, value) {
+          if (err) return next(err);
+          next(null, value);
+        });
+      },
+      initialReport: function (next) {
+        estimateInitialReport(augur, universe, auth, timestamp, function (err, value) {
+          if (err) return next(err);
+          next(null, value);
+        });
+      },
       binaryMarket: function (next) {
         estimateCreateBinaryMarket(augur, universe, auth, timestamp, function (err, value) {
           if (err) return next(err);
